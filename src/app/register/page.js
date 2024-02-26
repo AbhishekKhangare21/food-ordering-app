b3d5684f8844245ce8c780ec67ef035f7139c196
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-02-26 20:05:19 +0530

    Update package versions and add missing dependencies

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,13 +1,36 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 const RegisterPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    fetch("/api/register", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
   return (
     <section className="mt-8">
       <h1 className="text-center text-primary text-4xl mb-4">Register</h1>
-      <form className="block max-w-sm mx-auto">
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="password" />
+      <form className="block max-w-sm mx-auto" onSubmit={handleFormSubmit}>
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button type="submit">Register</button>
         <div className="my-4 text-center text-gray-500">
           or login with privider
