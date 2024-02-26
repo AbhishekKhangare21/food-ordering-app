69cb47fa67b438a610a3a9696733ac397e5ba128
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-02-26 17:22:15 +0530

    added 'About us' and 'Contact us' sections to the homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Header from "./components/layout/Header";
 import Hero from "./components/layout/Hero";
 import HomeMenu from "./components/layout/HomeMenu";
+import SectionHeaders from "./components/layout/SectionHeaders";
 
 export default function Home() {
   return (
@@ -8,6 +9,40 @@ export default function Home() {
       <Header />
       <Hero />
       <HomeMenu />
+      <section className="text-center my-16">
+        <SectionHeaders subHeader={"Our story"} mainHeader={"About us"} />
+        <div className="text-gray-500 max-w-md mx-auto mt-4 flex flex-col gap-4">
+          <p>
+            Lorem ipsum dolor sit amet consectetur sit amet consectetur
+            adipisicing elit. Dolore eum harum tenetur delectus quae debitis
+            modi at assumenda. Repudiandae, maxime.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore eum
+            harum tenetur delectus quae debitis modi at assumenda. Repudiandae,
+            maxime. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Dolore eum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore eum
+            harum.
+          </p>
+        </div>
+      </section>
+      <section className="text-center my-8">
+        <SectionHeaders
+          subHeader={"Don't hesitate"}
+          mainHeader={"Contact us"}
+        />
+        <div className="mt-8">
+          <a className="text-4xl underline text-gray-50" href="tel:+4684928922">
+            +46 762 348 123
+          </a>
+        </div>
+      </section>
+      <footer className="border-t p-8 text-center text-gray-500 mt-16">
+        &copy; 2024 All rights reserved
+      </footer>
     </>
   );
 }
