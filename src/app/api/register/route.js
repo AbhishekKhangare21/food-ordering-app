074b48f8c407f6fed9ace726c8d1a4d36d4e1473
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-02-26 21:13:38 +0530

    Update register route to use MongoDB

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,3 +1,9 @@
-export function POST(req, res) {
-  return Response.json("ok");
+import mongoose from "mongoose";
+import { User } from "../../models/User";
+
+export async function POST(req, res) {
+  const body = await req.json();
+  mongoose.connect(process.env.MONGO_URL);
+  const createdUser = await User.create(body);
+  return Response.json(createdUser);
 }
