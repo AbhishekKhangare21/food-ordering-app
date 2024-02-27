b373a4e4dbc46caddf86d0b724ac5bc8dc6fb026
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-02-27 22:15:35 +0530

    Add user name to header and change logout button to a link to profile page

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,9 +4,12 @@ import Link from "next/link";
 
 const Header = () => {
   const session = useSession();
-  console.log(session);
-  const status = session.status;
-
+  const status = session?.status;
+  const userData = session.data?.user;
+  let userName = userData?.name || userData?.email;
+  if (userName && userName.includes("@")) {
+    userName = userName.split("@")[0];
+  }
   return (
     <header className="flex items-center justify-between py-2">
       <Link href={"/"} className="text-primary font-semibold text-2xl">
@@ -20,12 +23,17 @@ const Header = () => {
       </nav>
       <nav className="flex items-center gap-8 text-gray-500 font-semibold">
         {status === "authenticated" && (
-          <button
-            onClick={() => signOut()}
-            className="bg-primary rounded-full text-white px-8 py-2"
-          >
-            Logout
-          </button>
+          <>
+            <Link href={"/profile"} className="whitespace-nowrap">
+              Hello, {userName}
+            </Link>
+            <button
+              onClick={() => signOut()}
+              className="bg-primary rounded-full text-white px-8 py-2"
+            >
+              Logout
+            </button>
+          </>
         )}
         {status === "unauthenticated" && (
           <>
