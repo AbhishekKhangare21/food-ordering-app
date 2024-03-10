a61c38f1a5020a7cb994cc8d91bea3984588c496
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-03-10 06:15:00 +0530

    Remove pizza image from menu item

--- src/components/menu/MenuItem.js
+++ src/components/menu/MenuItem.js
@@ -4,11 +4,11 @@ const MenuItem = () => {
   return (
     <div className="bg-gray-200 p-4 rounded-lg text-center group hover:bg-white hover:shadow-md hover:shadow-black/25 hover:transition-all transition-all ">
       <div className="text-center max-h-8"></div>
-      <img
+      {/* <img
         src="/pizza.png"
         className="max-h-auto max-h-32 block mx-auto"
         alt="pizza"
-      />
+      /> */}
       <h4 className="font-semibold text-xl my-3">Pepperoni Pizza</h4>
       <p className="text-gray-500 text-sm">
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
